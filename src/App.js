b476d2bb861beb5fd035cbbc76d0d1268497d931
Steven <steven.camillieri@protonmail.com>
2022-03-11 01:00:09 -0500

    building components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from "react";
-import "./App.css";
-import Header from "./components/Header/Header";
-import Countdown from "./components/Countdown/Countdown";
-import Socials from "./components/Socials/Socials";
+// import "./App.css";
+// import Header from "./components/Header/Header";
+// import Countdown from "./components/Countdown/Countdown";
+// import Socials from "./components/Socials/Socials";
+// import Product from "./components/Products/Product";
+import Products from "./components/Products/Products";
 
 function App() {
-
   return (
     <main className="appWrap">
-      <Header />
-      <Countdown />
-      <Socials />
+      <Products />
     </main>
   );
 }
