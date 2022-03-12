c5133638e58159c63caedbe2457e1a977345234c
Steven <steven.camillieri@protonmail.com>
2022-03-12 12:34:20 -0500

    photo array done

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -4,7 +4,12 @@ import "./Portfolio.css";
 
 export default function Portfolio() {
   const photoArr = Photos.map((i) => {
-    return <img key={i.id} src={i.src} alt={i.name}></img>;
+    return (
+      <div>
+        <img key={i.id} src={i.src} alt={i.alt}></img>
+        <p>{i.name}</p>
+      </div>
+    );
   });
 
   console.log(photoArr);
